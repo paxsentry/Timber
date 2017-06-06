1477030f592a43f0b3b750688f32cd62d546ebdd
PaxSentry <paxsentry@gmail.com>
2017-06-06 19:03:00 +0100

    Background loading

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -1,6 +1,31 @@
 #include "stdafx.h"
+#include <SFML\Graphics.hpp>
 
 int main()
 {
+    sf::VideoMode vm(1920, 1080);
+    sf::RenderWindow window(vm, "Timber");
+
+    sf::Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+
+    sf::Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0, 0);
+
+    while (window.isOpen())
+    {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            window.close();
+        }
+
+        window.clear();
+
+        window.draw(spriteBackground);
+
+        window.display();
+    }
+
     return 0;
 }
